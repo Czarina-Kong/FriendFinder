9c18951774fa6b03af4021cd8b0db42efa7b1fb1
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-02-04 22:25:53 -0800

    link api and html pages using routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,8 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
 var friends = [
 {
   "name":"Chris Pratt",
@@ -5,28 +10,28 @@ var friends = [
   "scores":[
       4, 2, 5, 1, 5, 1, 2, 1, 2, 5
     ]
-}
+},
 {
   "name":"Tom Hardy",
   "photo":"https://media.giphy.com/media/AulPWu750BnYA/giphy.gif",
   "scores":[
       5, 4, 4, 4, 1, 3, 5, 3, 1, 1
     ]
-}
+},
 {
   "name":"Lee Jong-suk",
   "photo":"https://media.giphy.com/media/WW0HCQm8mOAcU/giphy.gif",
   "scores":[
       2, 4, 3, 4, 3, 4, 1, 4, 4, 2
     ]
-}
+},
 {
   "name":"Idris Elba",
   "photo":"https://media.giphy.com/media/FyKfseNB6a8tG/giphy.gif",
   "scores":[
       3, 3, 3, 3, 3, 1, 2, 4, 2, 3
     ]
-}
+},
 {
   "name":"Gaston",
   "photo":"https://media.giphy.com/media/pF8sfgvZvDKPm/giphy.gif",
@@ -35,3 +40,6 @@ var friends = [
     ]
 }
 ];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
