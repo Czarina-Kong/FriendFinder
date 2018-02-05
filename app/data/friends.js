654afd57280ea0fe230471f17705aafe9a2f6ddc
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-02-05 00:55:30 -0800

    code best friend finder logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,35 +8,35 @@ var friends = [
   "name":"Chris Pratt",
   "photo":"https://media.giphy.com/media/Z85WGWaAhSS3e/giphy.gif",
   "scores":[
-      4, 2, 5, 1, 5, 1, 2, 1, 2, 5
+      '4', '2', '5', '1', '5', '1', '2', '1', '2', '5'
     ]
 },
 {
   "name":"Tom Hardy",
   "photo":"https://media.giphy.com/media/AulPWu750BnYA/giphy.gif",
   "scores":[
-      5, 4, 4, 4, 1, 3, 5, 3, 1, 1
+      '5', '4', '4', '4', '1', '3', '5', '3', '1', '1'
     ]
 },
 {
   "name":"Lee Jong-suk",
   "photo":"https://media.giphy.com/media/WW0HCQm8mOAcU/giphy.gif",
   "scores":[
-      2, 4, 3, 4, 3, 4, 1, 4, 4, 2
+      '2', '4', '3', '4', '3', '4', '1', '4', '5', '2'
     ]
 },
 {
   "name":"Idris Elba",
   "photo":"https://media.giphy.com/media/FyKfseNB6a8tG/giphy.gif",
   "scores":[
-      3, 3, 3, 3, 3, 1, 2, 4, 2, 3
+      '2', '5', '3', '5', '2', '2', '1', '4', '5', '4'
     ]
 },
 {
   "name":"Gaston",
   "photo":"https://media.giphy.com/media/pF8sfgvZvDKPm/giphy.gif",
   "scores":[
-      1, 5, 5, 5, 1, 5, 3, 5, 3, 5
+      '1', '5', '5', '5', '1', '5', '3', '5', '3', '5'
     ]
 }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,23 +29,47 @@ module.exports = function(app) {
   // Then the server saves the data to the friends array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    friends.push(req.body);
-    res.json(true);
+  app.post('/api/friends', function(req, res) {
+    var userData = req.body
+    // console.log('userScores = ' + JSON.stringify(userData));
+
+// Determine the user's most compatible friend using the following as a guide:
+// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+// Example:
+// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+// Total Difference: 2 + 1 + 2 = 5
+// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
+// The closest match will be the user with the least amount of difference.
+// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+// The modal should display both the name and picture of the closest match.
+// ------------------------------------------------------------------------------
+    var userScores = userData.scores
+    var matchName = ''
+    var matchImage = ''
+    var comparison = 1000
+// loop through all friends
+    for (var i = 0; i < friends.length; i++) {
+      var difference = 0
+// loop through all scores and set var difference = absolute val of friends.scores-userScores
+      for (var j = 0; j < userScores.length; j++) {
+        difference += Math.abs(friends[i].scores[j] - userScores[j]);
+      }
+      // console.log('difference = ' + difference)
+// set lowest difference to comparison, store friend name and photo
+      if (difference < comparison) {
+        // console.log('Closest match found = ' + difference);
+        // console.log('Friend name = ' + friends[i].name);
+        // console.log('Friend image = ' + friends[i].photo);
+        comparison = difference
+        matchName = friends[i].name
+        matchImage = friends[i].photo
+      }
+    }
+// Add new user
+    friends.push(userData)
+// Send appropriate response
+    res.json({status: 'OK', name: matchName, photo: matchImage})
   });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don't worry about it!
-
-  // app.post("/api/clear", function() {
-  //   // Empty out the arrays of data
-  //   tableData = [];
-  //   waitListData = [];
-
-  //   console.log(tableData);
-  // });
 };
